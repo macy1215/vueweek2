144b8354da751bfe56fe5040bf12b7c178cfb99f
Macy Huang <mjmj3794@gmail.com>
2024-01-24 23:33:20 +0800

    完成功能

--- productlist.js
+++ productlist.js
@@ -1,6 +1,7 @@
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 
 let myModal = '';
+let delProductModal='';
 
 
 const app = createApp({
@@ -9,7 +10,10 @@ const app = createApp({
             apiUrl: `https://vue3-course-api.hexschool.io/v2`,
             path:'maciw2',
             products:[], // 資料集
-            tempProduct:{},//暫存區
+            isNew: false, //是否新增
+            tempProduct:{
+                imagesUrl: [],
+            },//暫存區
         };
     },
     methods: {
@@ -36,18 +40,71 @@ const app = createApp({
        addImgurl(){ 
             console.log('新增圖片')
         },
-       removeImgurl(){
-            console.log('刪除圖片')
+       removeImgurl(key){
+            console.log(key)
        },
        showProduct(item){
         this.tempProduct=item;
        },
-       openModal(){
-        myModal.show();
+       openModal(isNew, item){
+        if(isNew === 'new'){
+            this.tempProduct={
+                imagesUrl:[],
+                //新增，將所有欄位淨空
+            };
+            this.isNew=true;
+            myModal.show();
+        }else if(isNew === 'edit') {
+            this.tempProduct={...item};//將值帶入input
+            this.isNew=false;
+            myModal.show();
+        }else if(isNew === 'delete'){
+            this.tempProduct={...item};
+            delProductModal.show();//跳出警告視窗
+        }
+
+        //myModal.show();
        },
-       saveModal(){
-        myModal.hide();
+       updateProduct(){ //更新與新增使用同一個 method
+            let url = `${this.apiUrl}/api/${this.path}/admin/product`
+            let http = 'post' //傳入資料 
+
+            if(!this.isNew){//當確認為新增時，更新資料。這個是編輯的不是新增
+                url = `${this.apiUrl}/api/${this.path}/admin/product/${this.tempProduct.id}`
+                http = 'put' //更新資料
+            }
+
+            axios[http](url,{data:this.tempProduct})
+                .then((res)=>{
+                    alert(res.data.message);
+                    myModal.hide();
+                    this.getData();
+                })
+                .catch((err)=>{
+                    alert(err.data.message);
+                    console.log(err);
+                })
        },
+       createImages(){
+        //新增圖片。將陣列中資料清空，新增新資料
+         this.tempProduct.imagesUrl=[];
+         this.tempProduct.imagesUrl.push('');
+       },
+       removeImgurl(){
+        
+        const url = `${this.apiUrl}/api/${this.path}/admin/product/${this.tempProduct.id}`;
+        
+        axios.delete (url,{data:this.tempProduct})
+                .then((res)=>{
+                    alert(res.data.message);
+                    delProductModal.hide();
+                    this.getData();
+                })
+                .catch((err)=>{
+                    alert(err.data.message);
+                    console.log(err);
+                })
+       }
     },
     mounted(){
         //token
@@ -59,6 +116,7 @@ const app = createApp({
     
         this.checkAdmin();
         myModal=new bootstrap.Modal(document.querySelector('#productModal'));
+        delProductModal=new bootstrap.Modal(document.querySelector('#delProductModal'));
     }
 });
 
